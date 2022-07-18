f0fe20b5b61d80af1a0adf2af1b5b7d08b2a08c2
Sipho Mancam <sipho.mancam@rbconsult.co.za>
2022-07-18 10:52:24 +0200

    Optimisation

--- Backend/Webserver/App.py
+++ Backend/Webserver/App.py
@@ -31,6 +31,9 @@ package_table = {
 def index_data():
     return package_table
 
+@app.route('/')
+def index():
+    return "Hello world"
 
 if __name__ == '__main__':
     app.run(host=CONSTANTS.HOST, debug=True)
